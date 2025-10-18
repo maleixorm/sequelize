0d973157ededf8200361d62d501b4548465b895f
Marcos Aleixo <maknytz@gmail.com>
2025-10-18 01:16:52 -0300

    Criando o arquivo de conexão com o sequelize

---
+++ db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('nodesequelize', 'php', '123456', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+try {
+    sequelize.authenticate()
+    console.log('Conectamos com sucesso com o Sequelize')
+} catch (err) {
+    console.log('Não foi possível conectar: ', err)
+}
+
+module.exports = sequelize
