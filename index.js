2f47da3ebfcae01ccc08198072f63307310a826a
Marcos Aleixo <maknytz@gmail.com>
2025-10-20 14:47:30 -0300

    Criando a rota para visualizar os dados do usuário através do id

--- index.js
+++ index.js
@@ -39,6 +39,12 @@ app.post('/users/create', async (req, res) => {
     res.redirect('/')
 })
 
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id
+    const user = await User.findOne({ raw: true, where: { id: id }})
+    res.render('userview', { user })
+})
+
 app.get('/', async (req, res) => {
     const users = await User.findAll({raw:true})
     res.render('home', { users: users })
