cebe5f947a6b3ec60a0397b81e50ae3ad1063cd9
Marcos Aleixo <maknytz@gmail.com>
2025-11-02 15:27:33 -0300

    Criando a rota delete

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ app.get('/users/:id', async (req, res) => {
     res.render('userview', { user })
 })
 
+app.post('/users/delete/:id', async (req, res) => {
+    const id = req.params.id
+    await User.destroy({ where: {id: id } })
+    res.redirect('/')
+}) 
+
 app.get('/', async (req, res) => {
     const users = await User.findAll({raw:true})
     res.render('home', { users: users })
